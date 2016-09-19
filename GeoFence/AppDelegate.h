69759169377bca08bf795af7f4f79a1cb39d740d
Love <Love@love.local>
2016-09-19 18:52:32 +0530

    Initial Commit

---
+++ GeoFence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeoFence
+//
+//  Created by Love on 9/19/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeoFence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeoFence
+//
+//  Created by Love on 9/19/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
